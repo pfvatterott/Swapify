c06af1993843b52bb4f171c7b16e2ff7e630c485
Paul Vatterott <pfvatterott@gmail.com>
2021-05-08 14:19:07 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import GoogleLogin from 'react-google-login'
+
 
 function App() {
+
+  function responseGoogle(response) {
+    console.log(response)
+    console.log(response.profileObj)
+  }
+
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <GoogleLogin 
+      clientId="582713087977-59mpjc19jph6a912mbsb4l41ak8msu56.apps.googleusercontent.com"
+      buttonText=""
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+      
+      />
     </div>
   );
 }

a404b72007c59188132da6db18060e70189e26fd
Paul Vatterott <pfvatterott@gmail.com>
2021-05-11 13:19:14 -0600

    user can drag and drop items into box

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import "filepond/dist/filepond.min.css";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- client/src/pages/createItem.js
+++ client/src/pages/createItem.js
@@ -3,12 +3,26 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import axios from 'axios';
+import { FilePond, registerPlugin } from 'react-filepond';
+
+
+// Import FilePond styles
+
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+
 
 function Item() {
   // Setting our component's initial state
   const [selectedFile, setFile] = useState([])
-  const [desciption, setDescription]= useState([]);
+  const [description, setDescription]= useState([]);
   const [formObject, setFormObject] = useState({})
+
+  const [files, setFiles] = useState([])
+
 
   // 
   useEffect(() => {
@@ -16,23 +30,23 @@ function Item() {
   }, [])
 
   //sets the selectedFile state when a a user drops in a file.
-  handleFileChange=(event)=> {
-    console.log(event);
-    const { name, value } = event.target.files[0];
-    this.setState({selectedFile:event.target.files[0]});
+  // handleFileChange=(event)=> {
+  //   console.log(event);
+  //   const { name, value } = event.target.files[0];
+  //   this.setState({selectedFile:event.target.files[0]});
 
-  };
+  // };
 
 
-  handleDescriptionChange=(event)=> {
-    console.log(event.value);
-    this.setState({description:event.target.value});
+  // handleDescriptionChange=(event)=> {
+  //   console.log(event.value);
+  //   this.setState({description:event.target.value});
    
-  };
+  // };
 
   
   function fileUploadHandler() {
-    event.preventDefault();
+    // event.preventDefault();
    //This is where we post to a DB axios.post()- or we need to put it into the API and call that.
 
    
@@ -42,12 +56,12 @@ function Item() {
 
             <form>
               <Input
-                onChange={handleFileChange}
+                // onChange={handleFileChange}
                 type="file"
                 placeholder="Upload a photo"
               />
               <TextArea
-                onChange={handleDescriptionChange}
+                // onChange={handleDescriptionChange}
                 name="description"
                 placeholder="Add a description"
               /> 
@@ -57,6 +71,16 @@ function Item() {
               >
                 Upload
               </FormBtn>
+              <FilePond
+                files={files}
+                onupdatefiles={setFiles}
+                allowMultiple={true}
+                maxFiles={3}
+                server="/api"
+                name="files" 
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+              
+            ></FilePond>
             </form>
          
   

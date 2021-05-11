b040194bf15be69c306236072e7cb51a477066bc
Paul <pfvatterott@gmail.com>
2021-05-10 20:06:40 -0600

    if user tries to access profile page without logging in they are redirected to login page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,17 +1,25 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ArticleContext from "../utils/ArticleContext"
+import {Redirect} from 'react-router-dom';
 
 
 function Profile() {
-
     const {userState, handleSetUser } = useContext(ArticleContext);
-    console.log(userState)
+    const [redirect, setRedirect] = useState(false);
+
+
+    useEffect(() => {
+        if (userState.length === 0) {
+            console.log('no user')
+            setRedirect(true)
+        }
+    })
     return (
         <div>
+            { redirect ? (<Redirect push to="/"/>) : null };
             <h2>Profile Page</h2>
         </div>
     )
-
 }
 
 export default Profile;

548ada940b7ea981d5ced73d246c16a4ea2b3e39
Paul <pfvatterott@gmail.com>
2021-05-13 07:56:18 -0600

    image and buttons

--- client/src/pages/swipping.js
+++ client/src/pages/swipping.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Link, Redirect } from 'react-router-dom'
 import API from "../utils/API";
+import "./style.css"
 
 
 
 function Swipping() {
     const [redirect, setRedirect] = useState(false);
     const [notUserItems, setNotUserItems] = useState([])
+    const [currentItem, setCurrentItem] = useState([])
+    let imageNumber = 0;
 
     useEffect(() => {
         const userData = JSON.parse(localStorage.getItem('userData'))
@@ -19,18 +22,27 @@ function Swipping() {
                 item.itemOwner !== +userId
             ))
             setNotUserItems(notUserItemsArray)
-        })
-        
-    })
+            setCurrentItem(notUserItemsArray[imageNumber])
+        })    
+    }, [])
+
+    function handleItemLike() {
+        imageNumber++
+        setCurrentItem(notUserItems[imageNumber])
+    }
+
+
     return (
         <div>
             { redirect ? (<Redirect push to="/"/>) : null }
             <h2>Swipping</h2>
-            <ol>
-                {notUserItems.map(item => (
-                    <a href={item.imageURL}><li>{item.itemName}</li></a>
-                ))}
-            </ol>
+            <h4>{currentItem.itemName}</h4>
+            <h5>{currentItem.itemDescription}</h5>
+            <img class="itemImage" src={currentItem.imageURL}/>
+            <button onClick={handleItemLike}>Not Interested</button>
+            <button>Interested</button>
+
+          
         </div>
     )
 }

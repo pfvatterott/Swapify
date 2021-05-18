93edf8b93e4031c960bd24524134d4e44b21a085
Paul <pfvatterott@gmail.com>
2021-05-18 14:57:16 -0600

    interval function now checks if there is an item 'deleteitem' set to true

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,47 @@ import Profile from "./pages/profile"
 import Chat from "./pages/chat"
 import createItem from "./pages/createItem"
 import Swipping from "./pages/swipping"
+import API from "./utils/API"
 
 
 function App() {
 
   const [userState, setUserState] = useState([]);
-
+  const userDataJSON = JSON.parse(localStorage.getItem('userData'))
+  
   function handleSetUser(userData) {
     setUserState(userData)
   }
+
+  setInterval(function () {
+    API.getUserItems(userDataJSON.googleId).then((response) => {
+      console.log(response)
+      for (let i = 0; i < response.data.length; i++) {
+        if (response.data[i].deleteItem === true) {
+          console.log('there is an item that might be deleted soon')
+        }
+        
+      }
+
+    })
+  }, 5000)
+
+  setInterval(function () {
+    API.getUserMatches(userDataJSON.googleId).then((response) => {
+        // checks if matches have been read or not by user2 (the user that was not swipping when the match was made)
+        for (let i = 0; i < response.data.length; i++) {
+            if ((response.data[i].item2Owner == userDataJSON.googleId) && (response.data[i].item2Read === false)) {
+                const matchData = {
+                    item2Read: true
+                }
+                API.updateUserMatch(response.data[i]._id, matchData).then((matchPutResponse) => {
+                    console.log(matchPutResponse)
+                    alert('You have a new matched Item!')
+                })
+            }
+        }
+    })
+}, 5000)
 
   return (
     <Router>

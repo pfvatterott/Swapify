de4763c8b24aa4e5451db77f1d53a37958de5670
Paul <pfvatterott@gmail.com>
2021-05-24 21:16:58 -0600

    cross yo fingers

---
+++ client/build/precache-manifest.a909993dfca4aca53a3e0437c41b3d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce46fbcea9bd6d69d37a33678bbb3f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e017e86ff17a1b3382f8",
+    "url": "/static/css/2.ec0e4fde.chunk.css"
+  },
+  {
+    "revision": "689713db821f7dda093c",
+    "url": "/static/css/main.714795d8.chunk.css"
+  },
+  {
+    "revision": "e017e86ff17a1b3382f8",
+    "url": "/static/js/2.95e36fc2.chunk.js"
+  },
+  {
+    "revision": "857b89ee851909eefa2f3c0a97096fed",
+    "url": "/static/js/2.95e36fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689713db821f7dda093c",
+    "url": "/static/js/main.e86ca531.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);

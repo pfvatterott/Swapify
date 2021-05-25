6413f957670ca3166f511f280d2f4258fe2a9cbd
Paul <pfvatterott@gmail.com>
2021-05-24 22:45:05 -0600

    user can no longer see their own items

---
+++ client/build/precache-manifest.27504b7b288e2cf28f9230886135c31e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101854199ddd8b4f6596e8e3cbafb24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a80dba59a89490357084",
+    "url": "/static/css/2.ec0e4fde.chunk.css"
+  },
+  {
+    "revision": "e67f4d97b4c97d6d84f5",
+    "url": "/static/css/main.714795d8.chunk.css"
+  },
+  {
+    "revision": "a80dba59a89490357084",
+    "url": "/static/js/2.6bcf0109.chunk.js"
+  },
+  {
+    "revision": "857b89ee851909eefa2f3c0a97096fed",
+    "url": "/static/js/2.6bcf0109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67f4d97b4c97d6d84f5",
+    "url": "/static/js/main.b092cab9.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
